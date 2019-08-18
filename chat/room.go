f98e12201971e65d4c7927ef28dbddefc251d888
kamillle <32205171+kamillle@users.noreply.github.com>
2019-08-18 12:10:53 +0900

    new fields added to room struct

--- chat/room.go
+++ chat/room.go
@@ -2,8 +2,17 @@
 
 package main
 
+// clientsの在室を管理する clients map は直接操作しない
+// clientの入退出は必ず join と leave チャネルを使って管理する
+// clientsのmapを直接操作するとメモリの破壊等が起きる可能性がある
 type room struct {
 	// 他のclientに転送するためのメッセージを保持するチャネル
 	// メッセージを受け取ったら全てのclientに対してメッセージを送信する
 	forward chan []byte
+	// ルーム(clients)に参加するためのチャネル
+	join chan *client
+	// ルーム(clients)から退室するためのチャネル
+	leave chan *client
+	// room内にいる全clientsが保持される
+	clients map[*client]bool
 }
