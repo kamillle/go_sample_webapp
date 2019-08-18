2c5ce3ebea6b2a0013520127e2c7c861d4144a5d
kamillle <32205171+kamillle@users.noreply.github.com>
2019-08-18 22:08:10 +0900

    1.4.1 make all test passed in tracer_test.go

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,7 +1,29 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // 大文字にしておくことで外部アクセスが可能になる。小文字はプライベート
 type Tracer interface {
-	// ...interface{} 型は任意の方の引数を0~複数個受け取れる
 	Trace(...interface{})
+}
+
+// ユーザーはこのAPIを利用して、tracerオブジェクトを受け取ることが可能
+// tracerオブジェクトはプライベートなため直接操作はできないが、パブリックなAPIを利用して
+// オブジェクトを受け取ることは可能
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+// こちらは小文字からスタートなのでプライベートな構造体となる
+type tracer struct {
+	out io.Writer
+}
+
+// ...interface{} 型は任意の方の引数を0~複数個受け取れる
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
