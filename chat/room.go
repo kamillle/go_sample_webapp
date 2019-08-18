9ac31caeb1949faad573fc9f7e65e4545b01e17d
kamillle <32205171+kamillle@users.noreply.github.com>
2019-08-18 12:31:10 +0900

    implement run method to room struct

--- chat/room.go
+++ chat/room.go
@@ -16,3 +16,35 @@ type room struct {
 	// room内にいる全clientsが保持される
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	// ループ処理。強制終了しない限り回り続ける。
+	// goroutine(バックグラウンド)で実行するため、他の処理がブロックされることはない
+	//
+	// ループ処理の中で join, leave, forward のチャネルを監視しており、メッセージが届くとcase文が評価される
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退出
+			// r.clients から client を削除(退出)する
+			delete(r.clients, client)
+			// client.send チャネルを閉じることで以後のメッセージの受信をしないことになる
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのclient(r.clients)にメッセージを送信する
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージ送信
+				default:
+					// メッセージ送信に失敗
+					delete(r.clients, clinet)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
